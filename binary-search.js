f7906e1d17da17806301f32d4f848005e605b795
satish-313 <pradhansatish53@gmail.com>
2022-03-26 20:35:17 +0530

    binary search march 26

---
+++ binary-search.js
@@ -0,0 +1,12 @@
+const search = function (nums, target) {
+  let l = 0;
+  let r = nums.length - 1;
+  let mid;
+  while (l <= r) {
+    mid = Math.floor((l + r) / 2);
+    if (nums[mid] === target) return mid;
+    else if (nums[mid] > target) r = mid - 1;
+    else l = mid + 1;
+  }
+  return -1;
+};
